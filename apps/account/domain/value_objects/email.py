4dd5bcd5ea6ee4ea4c43e47dded0c91cec157c7b
B-singularity <skil1489@naver.com>
2025-05-24 17:45:14 +0900

    test:account vo email

--- apps/account/domain/value_objects/email.py
+++ apps/account/domain/value_objects/email.py
@@ -1,9 +1,15 @@
+import re
+
 class Email:
     def __init__(self, address):
         if not address:
             raise ValueError("이메일 주소는 비어있을 수 없습니다.")
-        if '@' not in address or '.' not in address.split('@')[-1]:
+
+        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+        if not re.match(email_regex, address):
             raise ValueError("유효하지 않은 이메일 형식입니다.")
+
         if len(address) > 254:
             raise ValueError("이메일 주소는 최대 254자까지 가능합니다.")
         self.__address = address
